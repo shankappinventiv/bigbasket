2c1b111058cc8d924b41ad84c6522a0425d78f3f
Shashank N Mishra <shashank.mishra@appinventive.com>
2020-06-23 14:36:31 +0530

    veg updtae

---
+++ src/vegComponent/stepper.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+// import { render } from "react-dom";
+// import Header from "./header";
+
+// @ts-nocheck
+import Slider from "react-animated-slider";
+
+import "react-animated-slider/build/horizontal.css";
+import "normalize.css/normalize.css";
+import "./slider-animations.css";
+import "./styles.css";
+import banner1 from "../assets/fruitsAndVegitable/slide-1.jpg"
+import banner2 from "../assets/fruitsAndVegitable/slide-2.jpg"
+
+const content = [
+  {
+    title: "Vulputate Mollis Ultricies Fermentum Parturient",
+    description:
+      "Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis.",
+    button: "Read More",
+    image: banner1,
+    user: "Luan Gjokaj",
+    userProfile: "https://i.imgur.com/JSW6mEk.png"
+  },
+  {
+    title: "Tortor Dapibus Commodo Aenean Quam",
+    description:
+      "Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui.",
+    button: "Discover",
+    image: banner2,
+    user: "Erich Behrens",
+    userProfile: "https://i.imgur.com/0Clfnu7.png"
+  },
+  
+];
+
+ const Stepper = () => (
+  <div>
+    <Slider className="slider-wrapper">
+      {content.map((item, index) => (
+        <div
+          key={index}
+          className="slider-content"
+          style={{ background: `url('${item.image}') no-repeat center center` }}
+        >
+          <div className="inner">
+            <h1>{item.title}</h1>
+            <p>{item.description}</p>
+            <button>{item.button}</button>
+          </div>
+          <section>
+            <img src={item.userProfile} alt={item.user} />
+            <span>
+              Posted by <strong>{item.user}</strong>
+            </span>
+          </section>
+        </div>
+      ))}
+    </Slider>
+  </div>
+);
+
+export default Stepper
